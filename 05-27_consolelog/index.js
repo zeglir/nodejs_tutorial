7dae63c11b873b3ae54f896c8143073b58ae327a
zeglir <zeglir@jcom.zaq.ne.jp>
2021-11-10 11:37:34 +0900

    Section5-27 学習終了

---
+++ 05-27_consolelog/index.js
@@ -0,0 +1,11 @@
+console.log("Hello console.log");
+console.debug("Hello console.debug");
+console.info("Hello console.info");
+console.error("Hello console.error");
+console.warn("Hello console.warn");
+
+// オブジェクトのスタックトレースを出力する
+console.trace("Hello console.trace");
+
+// 書式付きでの出力
+console.log("Object: %o", {"hello" : "world"});
